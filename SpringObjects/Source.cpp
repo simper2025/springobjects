bd822207105777c7e7a046748764fe2864e70c0e
Dallan Simper <dallan.simper@gmail.com>
2025-01-13 08:42:15 -0700

    first day

---
+++ SpringObjects/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//using namespace std;
+
+int main() {
+
+	std::cout << "Hello world";
+
+	int num;
+
+	std::cin >> num;
+	
+	std::cout << "You typed number " << num;
+
+	num = num * 1.07;
+
+
+	return 27;
+}
