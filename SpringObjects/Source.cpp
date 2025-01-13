0bc949274dd3f5235d1ca475a6c7c469a7809498
Dallan Simper <dallan.simper@gmail.com>
2025-01-13 08:54:28 -0700

    html example

--- SpringObjects/Source.cpp
+++ SpringObjects/Source.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
-//using namespace std;
+using namespace std;
 
 int main() {
 
-	std::cout << "Hello world";
+	string header = "<html><head></head><body>";
+	string footer = "</body></html>";
+
+	ofstream output;
+	output.open("person.html");
+
 
 	int num;
 
-	std::cin >> num;
+	cin >> num;
 	
-	std::cout << "You typed number " << num;
+	//output << header << "<span>" << num << "</span>" << footer;
+	output << header << num << footer;
+	
+	output.close();
 
-	num = num * 1.07;
-
-
-	return 27;
+	return 0;
 }
