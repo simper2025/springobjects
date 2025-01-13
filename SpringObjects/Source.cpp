07eb6091550ee2d2be7381262db6f5cd97545015
Dallan Simper <dallan.simper@gmail.com>
2025-01-13 10:24:32 -0700

    functions and comments

--- SpringObjects/Source.cpp
+++ SpringObjects/Source.cpp
@@ -4,16 +4,22 @@
 
 using namespace std;
 
+/// <summary>
+/// 
+/// </summary>
+/// <returns></returns>
 int main() {
 
-	string header = "<html><head></head><body>";
+	string header = "<html>\n<head>\n</head>\n<body>\n";
 	string footer = "</body></html>";
 
+	//Open up the file for writing
 	ofstream output;
 	output.open("person.html");
 
 
 	int num;
+
 
 	cin >> num;
 	
@@ -22,5 +28,21 @@ int main() {
 	
 	output.close();
 
+	int b = AddsTwoNumber(12, 45);
+
 	return 0;
+}
+
+int AddsTwoNumber(int a, int b) {
+	return a + b;
+}
+
+
+/// <summary>
+/// Adds one to the parameter
+/// </summary>
+/// <param name="num1">some number</param>
+/// <returns>another number</returns>
+int foo(int num1) {
+	return num1 + 1;
 }
