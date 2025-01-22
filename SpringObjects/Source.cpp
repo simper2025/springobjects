61a79f9e5c67a2c0a61d59b08edd652b6e4d6b9a
Dallan Simper <dallan.simper@gmail.com>
2025-01-22 09:25:02 -0700

    structs

--- SpringObjects/Source.cpp
+++ SpringObjects/Source.cpp
@@ -20,7 +20,32 @@ void PrintBoard(int board[][8]) {
 
 }
 
+struct Point2d {
+	int X;
+	int Y;
+};
+
+struct Point3d {
+	int X;
+	int Y;
+	int Z;
+};
+
 int main() {
+
+	int x;
+	Point3d p3;
+
+	p3.X = 39;
+	p3.Y = 12;
+	p3.Z = 4;
+
+	Point2d p1 = { 1,2 };
+	//p1->X
+
+	Point2d* p4 = new Point2d;
+	p4->X 
+
 
 	int* arr2 = new int[5];
 	arr2[0] = 22;
