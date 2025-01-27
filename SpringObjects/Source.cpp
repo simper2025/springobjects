654d8cbf97219c6aefb3c613278985e65d3bcee4
Dallan Simper <dallan.simper@gmail.com>
2025-01-27 09:10:37 -0700

    square 3

--- SpringObjects/Source.cpp
+++ SpringObjects/Source.cpp
@@ -8,22 +8,15 @@ using namespace std;
 
 class Square {
 public:
-	int GetLength() {
-		return length;
-	}
-	void SetLength(int l) {
-		length = l;
-		area = l * l;
-	}
 	int GetArea() {
-		//return length * length;
-		return area;
+		return length * length;
+		//return area;
 	}
+	int length;
 
 private:
-	int length;
 	//string name;
-	int area;
+	//int area;
 };
 
 int main() {
@@ -31,10 +24,10 @@ int main() {
 	Square s1;
 	string s2;
 
-	s1.SetLength(7);
-	//s1.length = 12;
+	//s1.SetLength(7);
+	s1.length = 12;
 
-	cout << "length: " << s1.GetLength() << endl;
+	cout << "length: " << s1.length << endl;
 	cout << "area: " << s1.GetArea() << endl;
 
 	//cout << "arr2: " << arr2 << endl;
