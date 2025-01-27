694a4d0ed0eb8f0e8d1a5979524bc9a8c2ec9383
Dallan Simper <dallan.simper@gmail.com>
2025-01-27 09:21:47 -0700

    unit

--- SpringObjects/Source.cpp
+++ SpringObjects/Source.cpp
@@ -19,16 +19,44 @@ private:
 	//int area;
 };
 
+class Unit {
+public:
+	//Unit(){}
+	Unit(string n) {
+		name = n;
+		health = 10;
+		damage = 2;
+	}
+	string GetName() {
+		return name;
+	}
+	int getHealth() {
+		return health;
+	}
+	int GetDamage() {
+		return damage;
+	}
+	void takeDamage(int d){
+		health = health - d;
+	}
+
+private:
+	string name;
+	int health;
+	int damage;
+
+
+};
+
 int main() {
+	Unit u1("Bob");
+	Unit u2("Robert");
 
-	Square s1;
-	string s2;
+	cout << u1.GetName() << " health: " << u1.getHealth() << endl;
 
-	//s1.SetLength(7);
-	s1.length = 12;
+	u1.takeDamage(u2.GetDamage());
 
-	cout << "length: " << s1.length << endl;
-	cout << "area: " << s1.GetArea() << endl;
+	cout << u1.GetName() << " health: " << u1.getHealth() << endl;
 
 	//cout << "arr2: " << arr2 << endl;
 	//cout << "*arr2: " << *arr2 << endl;
