78c00b5360860fb4e73646745dafcd1c74a61dd8
Dallan Simper <dallan.simper@gmail.com>
2025-01-27 09:00:32 -0700

    first square

--- SpringObjects/Source.cpp
+++ SpringObjects/Source.cpp
@@ -4,72 +4,38 @@
 
 using namespace std;
 
-int AddsTwoNumber(int* a, int* b);
 
-void MemoryLeak() {
-	int* arr = nullptr;
-	const int size = 1000000;
-	for (int i = 0; i < size; i++) {
-		arr = new int[size];
-		*arr = i;
-		delete(arr);
+
+class Square {
+public:
+	int GetLength() {
+		return length;
 	}
-}
+	void SetLength(int l) {
+		length = l;
+	}
+	int GetArea() {
+		return length * length;
+	}
 
-void PrintBoard(int board[][8]) {
-
-}
-
-struct Point2d {
-	int X;
-	int Y;
-};
-
-struct Point3d {
-	int X;
-	int Y;
-	int Z;
+private:
+	string name;
+	int length;
+	//int area;
 };
 
 int main() {
 
-	int x;
-	Point3d p3;
+	Square s1;
 
-	p3.X = 39;
-	p3.Y = 12;
-	p3.Z = 4;
+	s1.SetLength(7);
+	cout << "length: " << s1.GetLength() << endl;
+	cout << "area: " << s1.GetArea() << endl;
 
-	Point2d p1 = { 1,2 };
-	//p1->X
-
-	Point2d* p4 = new Point2d;
-	p4->X = 12;
-
-
-	int* arr2 = new int[5];
-	arr2[0] = 22;
-
-	cout << "arr2: " << arr2 << endl;
-	cout << "*arr2: " << *arr2 << endl;
-	cout << "&arr2: " << &arr2 << endl;
-
-	delete(arr2);
-
-	cout << "arr2: " << arr2 << endl;
+	//cout << "arr2: " << arr2 << endl;
 	//cout << "*arr2: " << *arr2 << endl;
-	cout << "&arr2: " << &arr2 << endl;
+	//cout << "&arr2: " << &arr2 << endl;
+
 
 }
 
-int AddsTwoNumber(int* a, int* b) {
-	cout << "a: " << a << endl;
-	cout << "*a: " << *a << endl;
-	cout << "&a: " << &a << endl;
-	return *a + *b;
-}
-
-
-int foo(int num1) {
-	return num1 + 1;
-}
