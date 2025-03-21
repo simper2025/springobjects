373e7a7e77b4288697397a1f397f75bb1368801c
Dallan Simper <dallan.simper@gmail.com>
2025-03-21 08:51:37 -0600

    polymorphism

--- SpringObjects/SmallSource.cpp
+++ SpringObjects/SmallSource.cpp
@@ -11,11 +11,11 @@ public:
 	}
 };
 
-class Bird  {
+class Bird {
 private:
 	string color;
 	int height;
-public: 
+public:
 	Bird() {
 		color = "Brown";
 		height = 12;
@@ -25,25 +25,33 @@ public:
 		color = c;
 		//cout << "Bird(string)\n";
 	}
+
+	virtual string Taxonomy() = 0;
+
 	virtual string Speak() {
 		return "tweet";
 	}
 };
 
-class Rooster : public Bird{};
+class Rooster : public Bird {
+	string Taxonomy() { return "Birdicus Roosterian"; }
+};
 
 class Duck :public Bird {
-
+private:
+	string duckOnly;
 public:
 	Duck() :Bird() {
 		//cout << "Duck()\n";
+		duckOnly = "yes";
 	}
 	//virtual string Speak() override {
 	string Speak() {
 		return "quack";
 	}
+	string Taxonomy() { return "Birdicus Breadis"; }
 	void Swim() {
-		cout << "Duck goes swimming. \n";
+		cout << "Duck goes swimming. " << duckOnly << endl;
 	}
 };
 
@@ -56,27 +64,43 @@ public:
 };
 
 int main() {
-	
+
 	vector<Bird*> flock;
 
 	//cout << "make b1\n";
-	Bird * b1 = new Bird("orange");
+	//Bird * b1 = new Bird("orange");
 	//cout << "make d1\n";
 	Duck* d1 = new Duck();
-	FarmDuck* f1 = new FarmDuck();
-	Rooster* r1 = new Rooster();
+	Bird* f1 = new FarmDuck();
+	Bird* r1 = new Rooster();
 
-	flock.push_back(b1);
+	//flock.push_back(b1);
 	flock.push_back(d1);
 	flock.push_back(f1);
 	flock.push_back(r1);
 
+
 	for (int i = 0; i < flock.size(); i++)
 	{
-		cout << typeid(*flock[i]).name() << ": ";
-		cout << flock[i]->Speak() << endl;
+		Bird* tBird = flock[i];
+
+		cout << typeid(*tBird).name() << ": ";
+		cout << tBird->Speak() << endl;
+		
+		
+		
+
+
+		if (typeid(*tBird) == typeid(Duck)) {
+			cout << "I am a Duck\n";
+			Duck* tDuck = (Duck*) tBird;
+			tDuck->Swim();
+		}
 
 	}
+
+	Duck* tDuck = (Duck*)r1;
+	tDuck->Swim();
 
 	//cout << "make f1\n";
 	//FarmDuck f1;
@@ -86,7 +110,7 @@ int main() {
 	//cout << "f1 says: " << f1.Speak() << endl;
 	//f1.Swim();
 
-	for (int i = flock.size() - 1; i >=0 ; i--)
+	for (int i = flock.size() - 1; i >= 0; i--)
 	{
 		if (flock[i]) {
 			delete flock[i];
